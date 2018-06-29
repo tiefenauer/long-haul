a635ca9b12ed8c227a41514d4e823320c4848b3f
Daniel Tiefenauer <git@tiefenauer.info>
2018-06-29 07:51:50 +0200

    simplify game of nim

--- assets/img/posts/2018-06-28-programming-in-haskell/ch10.hs
+++ assets/img/posts/2018-06-28-programming-in-haskell/ch10.hs
@@ -1,3 +1,41 @@
+-- Hangman
+import System.IO
+
+match :: String -> String -> String
+match xs ys = [if elem x ys then x else '-' | x <- xs]
+
+play' :: String -> IO ()
+play' word =
+    do putStr "? "
+       guess <- getLine
+       if guess == word then
+           putStrLn "You got it!"
+       else
+           do putStrLn (match word guess)
+              play' word
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+sgetLine :: IO String
+sgetLine = do x <- getCh
+              if x == '\n' then
+                   do putChar x
+                      return []
+              else
+                   do putChar '-'
+                      xs <- sgetLine
+                      return (x:xs)
+
+hangman :: IO ()
+hangman = do putStrLn "Think of a word: "
+             word <- sgetLine
+             putStrLn "Try to guess it:"
+             play' word
+
 -- Game of nim (without input validation
 showBoard :: [Int] -> IO ()
 showBoard xs = do mapM_ putStrLn [replicate x '*' | x <- xs]
@@ -8,19 +46,17 @@ removeStars xs l n = [if i==l then x-n else x | (i,x) <- zip [1..] xs]
 play :: [Int] -> IO ()
 play board = do
     showBoard board
-    if sum board == 0 then 
+    if sum board == 0 then
         putStrLn "finished!"
     else do
         putStrLn "Enter line number: "
-        line <- getLine   
+        line <- getLine
         let l = (read line :: Int)
         putStrLn "Enter number of stars to remove: "
-        stars <- getLine  
+        stars <- getLine
         let n = (read stars :: Int)
         play (removeStars board l n)
 
-nim :: IO ()
-nim = do      
-    let board = reverse [1..5]             
-    play board
-         
+-- n :: Int == number of lines
+nim :: Int -> IO ()
+nim n = play (reverse [1..n])
